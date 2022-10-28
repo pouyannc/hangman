f3c2b01aac1c6182c861290cfc9896230c64cb10
Pouyan <pouyannc@gmail.com>
2022-10-28 18:05:12 -0400

    Allow user guess input

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,19 @@
 game_words = File.read('google-10000-words.txt').split.select {|word| word.length.between?(5,12) }
 
+secret_word = game_words.sample
+secret_word_length = secret_word.length
 
+lives = 5
+
+while lives > 0 do
+  puts 'Enter letter: '
+  loop do
+    letter_guess = gets.chomp
+    break if letter_guess.length == 1 && letter_guess.downcase =~ /\D/
+    puts 'Please enter a single letter.'
+  end
+
+  lives = 0
+end
+
+p secret_word
