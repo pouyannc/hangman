db5b46c8a81584366ac32531aba381e276b55e05
Pouyan <pouyannc@gmail.com>
2022-10-28 18:49:02 -0400

    Implement gameflow logic

--- hangman.rb
+++ hangman.rb
@@ -1,19 +1,37 @@
+def user_guess(letters_used, secret_word_letters_guessed)
+  loop do
+    letter = gets.chomp.downcase
+    if letters_used.any?(letter) || secret_word_letters_guessed.any?(letter)
+      puts 'That letter has been used already. Enter a new letter: '
+      next
+    end
+    return letter if letter.length == 1 && letter =~ /\D/
+    puts 'Please enter a single letter.'
+  end
+end
+
 game_words = File.read('google-10000-words.txt').split.select {|word| word.length.between?(5,12) }
 
 secret_word = game_words.sample
 secret_word_length = secret_word.length
-
+secret_word_letters_guessed = Array.new(secret_word_length, '_')
 lives = 5
+letters_used = []
 
 while lives > 0 do
-  puts 'Enter letter: '
-  loop do
-    letter_guess = gets.chomp
-    break if letter_guess.length == 1 && letter_guess.downcase =~ /\D/
-    puts 'Please enter a single letter.'
-  end
+  puts "\nLives remaining: #{lives}"
 
-  lives = 0
+  puts secret_word_letters_guessed.join(' ')
+
+  puts "\nEnter letter: "
+  letter_guess = user_guess(letters_used, secret_word_letters_guessed)
+
+  if secret_word.split('').any?(letter_guess)
+    secret_word.split('').each_with_index {|letter, i| secret_word_letters_guessed[i] = letter if letter_guess == letter }
+  else
+    letters_used.push(letter_guess)
+    lives -= 1
+  end
 end
 
 p secret_word
