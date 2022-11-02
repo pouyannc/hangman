ecdf98ce45060d30e47b75570b6ab2956df8f9da
Pouyan <pouyannc@gmail.com>
2022-11-02 00:32:45 -0400

    Functionality to save game to local files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hangman
+
+Hangman game on the command line.
+
+This games picks from nearly 10000 possible words and you have 7 lives to guess the correct word, one letter at a time. You may save and quit the game at any point by entering 'save'. You can choose to resume when you run the game again, or just start a new gome.
--- hangman.rb
+++ hangman.rb
@@ -58,14 +58,58 @@ class HangmanGame
     end
   end
 
+  def intro
+    puts "\nHangman!\nType 'save' at any point to save and quit\nPress enter to start"
+    gets
+  end
 
+  def load_game
+    puts "Enter 'r' to resume from last save or 'n' for new game"
+    loop do
+      response = gets.chomp.downcase
+      if response == 'r'
+        self.from_json
+        sleep 1
+        puts "...Game loaded"
+        sleep 1
+        break
+      elsif response == 'n'
+        break
+      else
+        puts "Invalid input. Enter either 'r' (resume) or 'n' (new game)"
+      end
+    end
+  end
+
+  def to_json
+    File.write('game_save.json', JSON.dump({
+      :secret_word => @secret_word, 
+      :secret_word_length => @secret_word_length, 
+      :secret_word_letters_guessed => @secret_word_letters_guessed, 
+      :lives => @lives, 
+      :letters_used => @letters_used
+    }))
+  end
+
+  def from_json
+    data = JSON.load(File.read('game_save.json'))
+    @secret_word = data['secret_word']
+    @secret_word_length = data['secret_word_length']
+    @secret_word_letters_guessed = data['secret_word_letters_guessed']
+    @lives = data['lives']
+    @letters_used = data['letters_used']
+  end
 
 end
 
-puts "Hangman!\nType 'save' at any point to save and quit\nPress enter to start"
-gets
-
 game = HangmanGame.new
+
+game.intro
+
+unless File.zero?('game_save.json')
+  game.load_game
+end
+
 game_end = false
 
 loop do
@@ -75,12 +119,18 @@ loop do
   letter_guess = game.user_guess
 
   if letter_guess == 'save'
+    game.to_json
+    sleep 1
     puts '...Game saved'
+    sleep 1
     break
   end
   
   game_end = game.update_display_info(letter_guess)
 
-  break if game_end == true
+  if game_end == true
+    File.write('game_save.json', '')
+    break
+  end
   
 end
